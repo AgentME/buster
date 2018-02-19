6fc204e0e0a815acca85090f95902439b7faff4f
Chris Cowan <agentme49@gmail.com>
2018-02-19 00:58:32 -0800

    Fix missing dependency.

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(name="buster",
       license="MIT",
       packages=["buster"],
       entry_points={"console_scripts": ["buster = buster.buster:main"]},
-      install_requires=['GitPython==0.3.2.RC1', 'async==0.6.1', 'gitdb==0.5.4', 'smmap==0.8.2', 'lxml>=3.4.1', 'beautifulsoup4>=4.3.2', 'argparse']
+      install_requires=['GitPython==0.3.2.RC1', 'async==0.6.1', 'gitdb==0.5.4', 'smmap==0.8.2', 'lxml>=3.4.1', 'beautifulsoup4>=4.3.2', 'html5lib', 'argparse']
     )
